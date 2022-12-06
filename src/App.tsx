47eeb1298712bb24b608da04b2db08b9d6e3309d
944172 <joao.nardes@compasso.com.br>
2022-12-05 22:05:36 -0400

    color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="Ima-poke">
-      <h1>Hello Poke!</h1>
+      <h1 className="text-4xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
     </div>
   );
 }
